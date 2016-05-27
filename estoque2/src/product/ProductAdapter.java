91925c86162ec3d8b6e81fd3ecf1786671aa95a4
dtandrade <dandarathamilys@gmail.com>
2016-05-27 10:47:33 -0300

    Modelagem e casos de testes de exclusão de produto

--- estoque2/src/product/ProductAdapter.java
+++ estoque2/src/product/ProductAdapter.java
@@ -3,16 +3,26 @@ package product;
 public interface ProductAdapter {
 	void appCloseSection(); 
 
+	/** This method will save the registered product and return a success message for the user
+	 * 
+	 * @return
+	 */
 	boolean appSaveProduct();
 	
-	/*O produto é válido se o sku for válido, 
-	 * se os componentes forem válidos 
-	 * e se a quantidade de componentes foram maior que 0*/
+	
+	/** This method must check if:
+	 * Product's sku is valid
+	 * Component is not null and valid
+	 * quantity of Component (Material) > 0
+	 */
 	boolean appValidadeProduct(); 
 	
-	/*Para checar se o sku do produto já foi cadastrado e se a quantidade
-	 * é maior que 0 para poder ser deletado do sistema*/
+	/** This method checks if product's sku is already registered and
+	 * if the quantity of product > 0 so it can be deleted from the system
+	 */
 	boolean appIsSkuValid();
 	
+	/** This method will delete the product and must return a message for the user */
+	boolean appDeleteData();
 	
 }
--- estoque2/src/product/ProductStateMachine.java
+++ estoque2/src/product/ProductStateMachine.java
@@ -1,14 +1,17 @@
 package product;
 
+import estoque2.State;
 
 public class ProductStateMachine extends java.lang.Object implements java.lang.Cloneable{
 	public Boolean statusProduct = false;
+	public Boolean skuProdValid = false;
 	public ProductState state;
 	private ProductAdapter adapter;
 	
 	public ProductStateMachine(){
 		state = ProductState.Idle;
 		statusProduct = false;
+		skuProdValid = false;
 	}
 	
 	public ProductStateMachine clone(){
@@ -24,6 +27,10 @@ public class ProductStateMachine extends java.lang.Object implements java.lang.C
 		if(in_colObject.length > 0){
 			
 			String sEventName = (String)in_colObject[0];
+			
+			/**
+			 * Register Product 
+			 * */
 			
 			if ((state == ProductState.Idle) && (sEventName.compareTo("registerEvent") == 0)){
 						
@@ -48,7 +55,38 @@ public class ProductStateMachine extends java.lang.Object implements java.lang.C
 			}else if((state == ProductState.Registered) && (sEventName.compareTo("finaliseEvent") == 0)){
 				closeSection();	
 			}
+		
+			/**
+			 * Delete Product 	
+			 */
+			if((state == ProductState.Idle) && (sEventName.compareTo("deleteEvent") == 0)){
+				state = ProductState.Input;
+				
+			}else if((state == ProductState.Input) && (sEventName.compareTo("okEvent") == 0)){
+				skuProdValid = isSkuProdValid();
+				
+				if(skuProdValid == true){
+					state = ProductState.Confirm;
+				}else{
+					state = ProductState.Input;
+				}
+				
+			}else if((state == ProductState.Confirm) && (sEventName.compareTo("confirmEvent") == 0)){
+				deleteData();
+				state = ProductState.Deleted;
+			}else if((state == ProductState.Confirm) && (sEventName.compareTo("cancelEvent") == 0)){
+				state = ProductState.Input;
+			}
 		}
+	}
+	
+	protected boolean deleteData(){
+		return adapter.appDeleteData();
+	}
+	
+	protected boolean isSkuProdValid(){
+		return adapter.appIsSkuValid();
+		
 	}
 	
 	protected void closeSection(){
